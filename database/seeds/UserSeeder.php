b70455d3d7cc73e6c16f324e86cae1b61149f5f2
MarioGafeed <jesushostingme@gmail.com>
2023-06-29 19:31:06 +0200

    add user seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
                 'firstname' => 'Mario',
                 'lastname' => 'Adel',
                 'username' => 'MarioAdel',
-                'email' => 'admin@example.com',
+                'email' => 'kero@gmail.com',
                 'password' => '$2y$10$IFj6SwqC0Sxrsiv4YkCt.OJv1UV4mZrWuyLoRG7qt47mseP9mJ58u',
                 'avatar' => 'no_avatar.png',
                 'phone' => '0123456789',
